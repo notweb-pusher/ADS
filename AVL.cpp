6237f77cac5bad06aa491c96dab1fc7a93324144
nikita <rxdvs.7@gmail.com>
2021-10-08 18:28:54 +0300

    fixed AVL, added erase

--- AVL.cpp
+++ AVL.cpp
@@ -4,11 +4,9 @@
 #include <vector>
 using namespace std;
 
-class AVLTree 
-{
+class AVLTree {
 public:
-  class Node 
-  {
+  class Node {
   public:
     Node() {}
     Node(int key) : key(key), height(0), left(nullptr), right(nullptr) {}
@@ -22,32 +20,86 @@ public:
 
   void insert(int key) { _root = insert(_root, key); }
 
-  void print() {
+  void print() { preorder(getRoot()); }
 
-    if (!_root)
-      return;
+  void erase(int value) { erase(_root, value); }
+  // BFS print
 
-    queue<Node *> queue;
-    queue.push(_root);
+  // void print() {
 
-    while (!queue.empty()) {
-        Node *tmp = queue.front();
-        cout << tmp->key << ' ';
+  //   if (!_root)
+  //     return;
 
-        if (tmp->left != nullptr)
-            queue.push(tmp->left);
-        if (tmp->right != nullptr)
-            queue.push(tmp->right);
+  //   queue<Node *> queue;
+  //   queue.push(_root);
 
-        queue.pop();
-    }
-  }
+  //   while (!queue.empty()) {
+  //       Node *tmp = queue.front();
+  //       cout << tmp->key << ' ';
+
+  //       if (tmp->left != nullptr)
+  //           queue.push(tmp->left);
+  //       if (tmp->right != nullptr)
+  //           queue.push(tmp->right);
+
+  //       queue.pop();
+  //   }
+  // }
 
 private:
-  Node *_root;
+  Node* _root;
+
+  Node*  erase(Node *node, int key) {
+    if (!node)
+      return nullptr;
+    if (node->key == key) {
+      // 0 children
+      if (!node->left && !node->right) {
+        delete node;
+        return nullptr;
+      }
+      // 1 right children
+      else if (!node->left) {
+        Node *tmp = node->right;
+        delete node;
+        return tmp;
+      }
+      // 1 left children
+      else if (!node->right) {
+        Node *tmp = node->left;
+        delete node;
+        return tmp;
+      }
+      // 2 children
+
+      else {
+        Node *tmp = successor(node->right);
+        node->key = tmp->key;
+        node->right = erase(node->right, node->key);
+      }
+    }
+
+    else if (node->key < key)
+      erase(node->right, key);
+    else if (node->key > key)
+      erase(node->left, key);
+
+    node->height = 1 + maxHeight(node);
+
+    rebalance(node, key);
+
+    return node;
+  }
+
+  // }
 
   Node *getRoot() { return _root; }
 
+  Node *successor(Node *node) {
+    while (node->left)
+      node = node->left;
+    return node;
+  }
   Node *insert(Node *node, int key) {
     if (node == nullptr) {
       return new Node(key);
@@ -62,6 +114,14 @@ private:
     }
 
     node->height = 1 + maxHeight(node);
+    
+    return rebalance(node, key);
+
+    // return node;
+  }
+
+  Node* rebalance(Node *node, int key) 
+  {
     int height_delta = getHeightDelta(node);
 
     if (height_delta > 1 && key < node->left->key) {
@@ -103,31 +163,32 @@ private:
 
     return tmp;
   }
-  
-  int maxHeight(Node *node) {
-    int left_height = node->left == nullptr ? 0 : node->left->height;
-    int right_height = node->right == nullptr ? 0 : node->right->height;
 
-    return left_height > right_height ? left_height : right_height;
+  int maxHeight(Node *node) {
+    return node ? max(height(node->left), height(node->right)) : 0;
   }
 
+  int height(Node *node) { return node ? node->height : 0; }
   int getHeightDelta(Node *node) {
-    if (!node)
-      return 0;
+    return node ? height(node->left) - height(node->right) : 0;
+  }
 
-    int left_height = node->left == nullptr ? 0 : node->left->height;
-    int right_height = node->right == nullptr ? 0 : node->right->height;
-
-    return left_height - right_height;
+  void preorder(Node *node) {
+    if (node) {
+      cout << node->key << " ";
+      preorder(node->left);
+      preorder(node->right);
+    }
   }
 };
 
 int main() {
-  vector<int> a = {1, 2, 3, 4, 5};
+  vector<int> a = {1, 2, 3, 4};
   AVLTree myTree;
 
   for (auto el : a)
     myTree.insert(el);
 
+  myTree.erase(2);
   myTree.print();
 }
